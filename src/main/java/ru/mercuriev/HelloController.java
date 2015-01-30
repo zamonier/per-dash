038e81f67399d36fc96a77581712fdf4ef625ef9
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2015-01-31 01:42:32 +0400

    add thymeleaf

--- src/main/java/ru/mercuriev/HelloController.java
+++ src/main/java/ru/mercuriev/HelloController.java
@@ -1,14 +1,20 @@
 package ru.mercuriev;
 
+import com.google.api.client.googleapis.auth.clientlogin.ClientLogin;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.io.IOException;
+
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 
 }
