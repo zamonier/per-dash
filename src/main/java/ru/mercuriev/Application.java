84336662386b14709bd78d64cb3dd7b1c223dcaa
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2015-03-17 22:43:38 +0300

    reformat

--- src/main/java/ru/mercuriev/Application.java
+++ src/main/java/ru/mercuriev/Application.java
@@ -1,11 +1,8 @@
 package ru.mercuriev;
 
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/ru/mercuriev/HelloController.java
+++ src/main/java/ru/mercuriev/HelloController.java
@@ -1,13 +1,7 @@
 package ru.mercuriev;
 
-import com.google.api.client.googleapis.auth.clientlogin.ClientLogin;
-import com.google.api.client.http.HttpResponseException;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.IOException;
 
 @Controller
 public class HelloController {
